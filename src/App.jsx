88c76bfd7121dece2b6dca9294f18fa4a7c8db4f
Jessica Fuller <jessicanfuller808@gmail.com>
2023-03-17 08:48:06 -0700

    added more file structure on the front end

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import { makeStyles } from '@mui/material/styles';
 
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
@@ -9,11 +11,21 @@ function App() {
 
   return (
     <div className="App">
-      <Typography variant='h1'>
+      <Typography variant="h1">
         Calorini
       </Typography>
+      <Typography variant="subtitle1">
+        Build your best body with simple, protein-packed recipes from around the web
+      </Typography>
+      <Button variant="contained">
+        Sign Up
+      </Button>
+      <Button variant="outlined">
+        Log In
+      </Button>
     </div>
   )
 }
+
 
 export default App
