41b2f977237f915f4eb793cada9c2153d13c264b
Jessica Fuller <jessicanfuller808@gmail.com>
2023-03-17 16:19:16 -0700

    installed tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,21 @@
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-
-
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
 
 
 function App() {
 
   return (
     <div className="App">
-        <Typography variant="h1">
+        <h1>
           Calorini
-        </Typography>
-        <Typography variant="subtitle1">
+        </h1>
+        <h3>
           Build your best body with simple, protein-packed recipes from around the web
-        </Typography>
-        <Button variant="contained">
+        </h3>
+        <button>
           Sign Up
-        </Button>
-        <Button variant="outlined">
+        </button>
+        <button>
           Log In
-        </Button>
+        </button>
     </div>
   )
 }
