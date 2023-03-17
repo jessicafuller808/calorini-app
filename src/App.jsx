bb3128b83ee95bfcb3aca8bc0d107a131e4e9457
Jessica Fuller <jessicanfuller808@gmail.com>
2023-03-17 09:54:13 -0700

    uninstalled emotion/react

--- src/App.jsx
+++ src/App.jsx
@@ -7,25 +7,25 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
+
 function App() {
 
   return (
     <div className="App">
-      <Typography variant="h1">
-        Calorini
-      </Typography>
-      <Typography variant="subtitle1">
-        Build your best body with simple, protein-packed recipes from around the web
-      </Typography>
-      <Button variant="contained">
-        Sign Up
-      </Button>
-      <Button variant="outlined">
-        Log In
-      </Button>
+        <Typography variant="h1">
+          Calorini
+        </Typography>
+        <Typography variant="subtitle1">
+          Build your best body with simple, protein-packed recipes from around the web
+        </Typography>
+        <Button variant="contained">
+          Sign Up
+        </Button>
+        <Button variant="outlined">
+          Log In
+        </Button>
     </div>
   )
 }
-
 
 export default App
