ab6deaae4831195b523cc259be7dddcf1a281642
Jessica Fuller <jessicanfuller808@gmail.com>
2023-03-17 21:55:29 -0700

    completed enough to CSS to have general feel of landing page

--- src/App.jsx
+++ src/App.jsx
@@ -3,25 +3,25 @@ import '../src/index.css'
 function App() {
 
   return (
-    <div className="App bg-[#101010] h-screen max-w-full text-slate-50 flex">
-      <div className="h-screen w-2/5 bg-[url('./assets/pablo.jpg')] bg-contain bg-no-repeat">
-      </div>
-      <section className="flex flex-col content">
-        <h1 className="text-6xl font-bold sans text-slate-50">
-          Calor<span className="text-lime-500">ini</span>
-        </h1>
-        <h3 className="sans">
-          Build your best body with simple, protein-packed recipes from around the web
-        </h3>
-        <div className="flex-row">
-          <button className="rounded-2xl bg-pink-900 w-20 h-8 font-semibold">
-            Sign Up
-          </button>
-          <button className="rounded-2xl border-slate-50 border-2 w-20 h-8 font-semibold">
-            Log In
-          </button>
+    <div className="App bg-[#101010] h-auto max-w-full text-slate-50 flex">
+        <div className="h-screen w-6/12 bg-[url('./assets/pablo.jpg')] bg-cover bg-no-repeat bg-center">
         </div>
-      </section>
+        <section className="w-3/12 flex flex-col justify-center items-end">
+          <h1 className="text-9xl font-bold sans text-slate-50 mb-4">
+            Calor<span className="text-lime-500">ini</span>
+          </h1>
+          <h2 className="serif text-2xl text-right mb-6">
+            Build your <span className='font-bold'>best body</span> with <span className='italic'>simple</span>, protein-packed recipes from around the web
+          </h2>
+          <div className="flex-row w-80 justify-end">
+            <button className="py-2 px-4 rounded-full bg-pink-900 w-32 h-12 font-semibold text-xl">
+              Sign Up
+            </button>
+            <button className="py-2 px-4 rounded-full border-slate-50 border-2 w-32 h-12 font-semibold text-xl ml-4">
+              Log In
+            </button>
+          </div>
+        </section>
     </div>
   )
 }
